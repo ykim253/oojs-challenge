8aeacdd46d41dbef5fce40c863c5c420de405ae6
ykim253 <ykim253@uw.edu>
2014-12-07 18:22:37 -0800

    finished

--- js/shapes.js
+++ js/shapes.js
@@ -13,3 +13,72 @@
     or registerClassicalShape() to register your new shapes with the application. See the
     app.js file for info on these functions.
  */
+function Rectangle (left, top, width, height, stylesMap) {
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.stylesMap = stylesMap;
+
+    this.renderShape = function(canvasCtx) {
+        canvasCtx.fillRect(this.left, this.top, this.width, this.height, this.stylesMap);
+    };
+}
+
+Rectangle.prototype = new Shape();
+
+registerClassicalShape('Rectangle', Rectangle);
+
+function Circle (left, top, width, height, stylesMap) {
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.stylesMap = stylesMap;
+
+    var radius = (width + height) /2;
+
+    this.renderShape = function(canvasCtx) {
+        canvasCtx.beginPath();
+        canvasCtx.arc(this.left, this.top, radius, 0, 2*Math.PI, this.stylesMap);
+        canvasCtx.fill();
+    };
+}
+
+Circle.prototype = new Shape();
+
+registerClassicalShape('Circle', Circle);
+
+var lightSaber = new Image();
+lightSaber.src = './img/starWars.jpg';
+var falcon = new Image();
+falcon.src = './img/falcon.jpg'
+var alternate = true;
+
+function starWars (left, top, width, height, stylesMap) {
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.stylesMap = stylesMap;
+
+    this.renderShape = function(canvasCtx) {
+        if (alternate == true) {
+            alternate = false;
+            console.log(alternate);
+            canvasCtx.drawImage(lightSaber, this.left, this.top);
+        } else {
+            alternate = true;
+            console.log(alternate);
+            canvasCtx.drawImage(falcon, this.left, this.top);
+        }
+
+    };
+}
+
+starWars.prototype = new Shape();
+
+registerClassicalShape('starWars', starWars);
+
+
+
